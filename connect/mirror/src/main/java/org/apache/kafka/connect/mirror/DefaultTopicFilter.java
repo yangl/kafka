3bf931cbb7d7bf2fe2cefaafd1c11443c7a8bc6e
YANGLiiN <yanglin5@sf-express.com>
2025-09-10 18:36:08 +0800

    feat(connect/mirror): 数据同步仅同步上下游集群都有的主题
    - 在 DefaultTopicFilter 中添加了对目标集群主题列表的缓存和定期刷新机制- 实现了根据目标集群主题存在性进行过滤的功能
    -增加了配置项以控制主题自动创建和刷新间隔
    - 优化了 shouldReplicateTopic 方法，考虑了主题过滤逻辑

--- connect/mirror/src/main/java/org/apache/kafka/connect/mirror/DefaultTopicFilter.java
+++ connect/mirror/src/main/java/org/apache/kafka/connect/mirror/DefaultTopicFilter.java
@@ -16,18 +16,27 @@
  */
 package org.apache.kafka.connect.mirror;
 
+import org.apache.kafka.clients.admin.Admin;
 import org.apache.kafka.common.config.AbstractConfig;
 import org.apache.kafka.common.config.ConfigDef;
 import org.apache.kafka.common.config.ConfigDef.Importance;
 import org.apache.kafka.common.config.ConfigDef.Type;
 import org.apache.kafka.common.utils.ConfigUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ExecutionException;
 import java.util.regex.Pattern;
+
+import static org.apache.kafka.connect.mirror.MirrorUtils.adminCall;
 
 /** Uses an include and exclude pattern. */
 public class DefaultTopicFilter implements TopicFilter {
-    
+    private static final Logger log = LoggerFactory.getLogger(DefaultTopicFilter.class);
+
     public static final String TOPICS_INCLUDE_CONFIG = "topics";
     private static final String TOPICS_INCLUDE_DOC = "List of topics and/or regexes to replicate.";
     public static final String TOPICS_INCLUDE_DEFAULT = ".*";
@@ -40,11 +49,35 @@ public class DefaultTopicFilter implements TopicFilter {
     private Pattern includePattern;
     private Pattern excludePattern;
 
+    private String sourceClusterAlias;
+    private ReplicationPolicy replicationPolicy;
+    private Admin targetAdminClient;
+    private long refreshTopicsIntervalMs = -1;
+
+    private volatile Set<String> targetTopics;
+    private volatile long lastRefreshTargetTopicsTimestamp = -1;
+
+
     @Override
     public void configure(Map<String, ?> props) {
         TopicFilterConfig config = new TopicFilterConfig(props);
         includePattern = config.includePattern();
         excludePattern = config.excludePattern();
+
+        Map<String, String> taskProps = new HashMap<>();
+
+        props.forEach((key, value) -> {
+            taskProps.put(key, (String) value);
+        });
+
+        MirrorSourceTaskConfig taskConfig = new MirrorSourceTaskConfig(taskProps);
+
+        sourceClusterAlias = taskConfig.sourceClusterAlias();
+        replicationPolicy = taskConfig.replicationPolicy();
+        targetAdminClient = taskConfig.forwardingAdmin(taskConfig.targetAdminConfig("topic-filter-target-admin"));
+        refreshTopicsIntervalMs = taskConfig.refreshTopicsInterval().minusSeconds(30).toMillis();
+
+        this.refreshTargetTopics();
     }
 
     private boolean included(String topic) {
@@ -57,7 +90,64 @@ public class DefaultTopicFilter implements TopicFilter {
 
     @Override
     public boolean shouldReplicateTopic(String topic) {
-        return included(topic) && !excluded(topic);
+        log.info("判断该主题是否要同步");
+
+        boolean contains = true;
+
+        boolean autoCreateTopicsEnabled = Boolean.parseBoolean(System.getProperty("MM2_AUTO_CREATE_TOPICS_ENABLE", "true"));
+        if (!autoCreateTopicsEnabled) {
+            String targetTopic = replicationPolicy.formatRemoteTopic(sourceClusterAlias, topic);
+
+            if (targetTopics == null || (refreshTopicsIntervalMs > 0 && System.currentTimeMillis() - lastRefreshTargetTopicsTimestamp > refreshTopicsIntervalMs)) {
+                this.refreshTargetTopics();
+            }
+
+            contains = targetTopics != null && targetTopics.contains(targetTopic);
+
+            if (!contains) {
+                log.info("下游集群没有该主题--{} -> {}", topic, targetTopic);
+            }
+
+        }
+
+        return included(topic) && !excluded(topic) && contains;
+    }
+
+    @Override
+    public void close() {
+        log.info("关闭时清理资源--targetAdminClient&targetTopics");
+
+        if (targetAdminClient != null) {
+            targetAdminClient.close();
+        }
+
+        if (targetTopics != null) {
+            synchronized (this) {
+                targetTopics.clear();
+                targetTopics = null;
+            }
+        }
+    }
+
+    private void refreshTargetTopics() {
+        long startTime = System.currentTimeMillis();
+
+        log.info("刷新下游集群主题列表开始");
+
+        try {
+            long now = System.currentTimeMillis();
+            Set<String> topics = adminCall(() -> targetAdminClient.listTopics().names().get(), () -> "list topics on target cluster");
+            synchronized (this) {
+                this.targetTopics = topics;
+                this.lastRefreshTargetTopicsTimestamp = now;
+            }
+
+        } catch (ExecutionException | InterruptedException e) {
+            log.warn("获取目标集群主题列表失败", e);
+        }
+
+        log.info("刷新下游集群主题列表结束, 耗时 {}", System.currentTimeMillis() - startTime);
+
     }
 
     static class TopicFilterConfig extends AbstractConfig {
